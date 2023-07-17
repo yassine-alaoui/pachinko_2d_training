9a2af82c892307c06fb7ba02aeff37ce9c159e85
yassine-alaoui <alaoui6b@gmail.com>
2023-07-17 11:17:38 +0100

    new project for learning

---
+++ Assets/scripts/random_moves.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random_moves : MonoBehaviour
+{
+    private int frames = 0;
+    private gameObject enemie;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (frames % 100 == 0)
+            enemie.
+
+       frames++;
+    }
+}
